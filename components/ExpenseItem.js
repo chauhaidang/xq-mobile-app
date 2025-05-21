2a77480b19f0a52e75de00de13d4272b20cebb5f
chauhaidang1 <chauhaidang1@gmail.com>
2025-05-21 22:36:22 +0700

    format datetime for expense item

--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -7,7 +7,7 @@ export default function ExpenseItem({ description, amount, date }) {
       <View style={styles.container}>
         <View>
           <Text style={[styles.textBase, styles.description]}>{description}</Text>
-          <Text style={styles.textBase}>${date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
           <Text style={styles.amount}>{amount}</Text>
